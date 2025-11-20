1fd54b4c56e41dc51ec784d70945f37e0520a52d
Luis Ángel Lobo Fuentes <166774713+Luis-AngelLF@users.noreply.github.com>
2025-11-20 17:11:36 -0600

    Refactor database connection function with comments

--- Proyecto/pruebas proyecto/dbconnection.py
+++ Proyecto/pruebas proyecto/dbconnection.py
@@ -2,13 +2,14 @@ import psycopg2
 def conectdatabase():
     try:
         connection=psycopg2.connect(
-            host='localhost',
+            host='localhost', #Acá si lo hosteamos habrá que cambiarlo
             user='postgres',
-            password='luis2006',
+            password='luis2006', #Acá hay que poner la contraseña de conección a la base de datos
             database='prueba-proyCripto'
         )
         #print(f'Conección exitosa a la base de datos{connection}')
         return connection
     except Exception as ex:
         print(f'Error de conección: {ex}')
+
         
